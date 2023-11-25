a6afa6faa06be3731c57c1dd86b44e9e4d16c311
Poleton <pol.fernandez.blanquez@hotmail.com>
2023-11-25 12:35:25 +0100

    Nothing

--- session-3/main.py
+++ session-3/main.py
@@ -1,46 +1,61 @@
 import torch
 from torch.utils.data import DataLoader
-
 from model import MyModel
-from utils import binary_accuracy
+from utils import binary_accuracy_with_logits  # Usando la función correcta para la precisión
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 
-
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
+device = torch.device("cuda" if torch.cuda.is_available() else torch.device("cpu"))
 
 def train_single_epoch(model, train_loader, optimizer):
     model.train()
     accs, losses = [], []
     for x, y in train_loader:
-        # You will need to do y = y.unsqueeze(1).float() to add an output dimension to the labels and cast to the correct type
-        ...
-        losses.append(loss.item())
-        accs.append(acc.item())
-    return np.mean(losses), np.mean(accs)
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+        outputs = model(x)
+        loss = F.binary_cross_entropy_with_logits(outputs, y.unsqueeze(1).float())
+        acc = binary_accuracy_with_logits(y, outputs)  # Usando la función correcta aquí
+        loss.backward()
+        optimizer.step()
 
+        losses.append(loss.item())
+        accs.append(acc)
+    return np.mean(losses), np.mean(accs)
 
 def eval_single_epoch(model, val_loader):
     accs, losses = [], []
     with torch.no_grad():
         model.eval()
         for x, y in val_loader:
-            ...
+            x, y = x.to(device), y.to(device)
+            outputs = model(x)
+            loss = F.binary_cross_entropy_with_logits(outputs, y.unsqueeze(1).float())
+            acc = binary_accuracy_with_logits(y, outputs)
+
             losses.append(loss.item())
-            accs.append(acc.item())
+            accs.append(acc)
     return np.mean(losses), np.mean(accs)
 
-
 def train_model(config):
-
-    data_transforms = transforms.Compose([...])
-    train_dataset = ImageFolder...
+    data_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.RandomResizedCrop(128, scale=(0.8, 1.0)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    train_dataset = ImageFolder(
+        "C:/Users/user/Desktop/MàsterIA/Lab/pycharm-aidl/aidl-2024-winter-mlops/session-3/cars_vs_flowers/training_set",
+        transform=data_transforms)
     train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
-    test_dataset = ImageFolder...
+
+    test_dataset = ImageFolder(
+        "C:/Users/user/Desktop/MàsterIA/Lab/pycharm-aidl/aidl-2024-winter-mlops/session-3/cars_vs_flowers/test_set",
+        transform=data_transforms)
     test_loader = DataLoader(test_dataset, batch_size=config["batch_size"])
 
     my_model = MyModel().to(device)
@@ -51,17 +66,13 @@ def train_model(config):
         print(f"Train Epoch {epoch} loss={loss:.2f} acc={acc:.2f}")
         loss, acc = eval_single_epoch(my_model, test_loader)
         print(f"Eval Epoch {epoch} loss={loss:.2f} acc={acc:.2f}")
-    
+
     return my_model
 
-
 if __name__ == "__main__":
-
     config = {
-        "lr": 1e-3,
-        "batch_size": 64,
-        "epochs": 5,
+        "lr": 3e-4,  # Tasa de aprendizaje ajustada
+        "batch_size": 32,  # Mantener o ajustar según sea necesario
+        "epochs": 15,  # Aumentar el número de épocas
     }
     my_model = train_model(config)
-
-    
--- session-3/model.py
+++ session-3/model.py
@@ -3,10 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MyModel(nn.Module):
-
     def __init__(self):
-        super().__init__()
-        ...
+        super(MyModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 64 * 64, 64)  # Simplificando la red
+        self.fc2 = nn.Linear(64, 1)
 
     def forward(self, x):
-        ...
+        x = self.pool(F.relu(self.conv1(x)))
+        x = x.view(-1, 16 * 64 * 64)
+        x = F.relu(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        return x
